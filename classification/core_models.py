347dc0cb39c902509fc7a9f25a43c8be48fc059e
MinaNguyen99 <hamyftlift1@gmail.com>
2024-01-13 23:56:57 +0700

    Add evaluation

--- classification/core_models.py
+++ classification/core_models.py
@@ -11,11 +11,15 @@ from imblearn.under_sampling import RandomUnderSampler
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn import metrics
-
+from joblib import dump
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.decomposition import TruncatedSVD
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import label_binarize
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.svm import SVC
 import numpy as np
@@ -23,9 +27,11 @@ import time
 import pandas as pd
 from xgboost import XGBClassifier
 import optuna
+import os
+from itertools import cycle
 
 
-class NewsClassification:
+class NewsClassificationModel:
 
     def __init__(self, data=None):
         self.data = data
@@ -40,10 +46,6 @@ class NewsClassification:
 
         tfidf_vectorizer = TfidfVectorizer()
         self.X = tfidf_vectorizer.fit_transform(self.data['content'])
-        # svd_vectorizer = TruncatedSVD(n_components=1, random_state=42)
-        # svd_vectorizer.fit(self.X)
-        # self.X = svd_vectorizer.transform(self.X)
-
         self.y = self.data['topic']
 
         oversampled_data = []
@@ -139,16 +141,184 @@ class NewsClassification:
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(text, label, test_size=test_size,
                                                                                 random_state=42)
 
+    def evaluation_model(self, name_model, classifier, train_time, label_encoder, fine_turning=False):
+        output_folder = '/home/sotatek/PycharmProjects/disertation_nlp/output'
+        # Predict on test set
+        y_pred = classifier.predict(self.X_test)
+        accuracy = metrics.accuracy_score(self.y_test, y_pred)
+        class_names = list(self.label_encoder.classes_)
+        report = metrics.classification_report(self.y_test, y_pred, target_names=class_names)
+        if fine_turning is False:
+            report_filename = os.path.join(output_folder, f"{name_model}_classification_report.txt")
+        else:
+            report_filename = os.path.join(output_folder, f"FT_{name_model}_classification_report.txt")
+        if fine_turning is False:
+            with open(report_filename, 'w') as file:
+                file.write(f"{name_model} Training Time: {train_time} seconds\n\n")
+                file.write(f'{name_model}, Accuracy ={accuracy}\n')
+                file.write("Classification Report:\n")
+                file.write(report)
+        else:
+            with open(report_filename, 'w') as file:
+                file.write(f"Best {name_model} Training Time: {train_time} seconds\n\n")
+                file.write(f'Best {name_model}, Accuracy ={accuracy}\n')
+                file.write("Classification Report:\n")
+                file.write(report)
+
+        # Create a Confusion Matrix of model and save it
+        labels = list(self.label_encoder.classes_)
+        cm = metrics.confusion_matrix(self.y_test, y_pred)
+        fig_size = max(8, len(labels) / 3)
+        plt.figure(figsize=(fig_size, fig_size))
+        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=labels,
+                    yticklabels=labels)
+        plt.title(f'Confusion Matrix for {name_model}')
+        plt.ylabel('True Label')
+        plt.xlabel('Predicted Label')
+
+        # Save confusion matrix
+        if fine_turning is False:
+            cm_image_filename = os.path.join(output_folder, f"{name_model}_confusion_matrix.png")
+        else:
+            cm_image_filename = os.path.join(output_folder, f"FT_{name_model}_confusion_matrix.png")
+        plt.savefig(cm_image_filename, bbox_inches='tight')
+        plt.close()
+
+        """ Create ROC Curve"""
+        # Binarize the labels for multi-class ROC curve
+        y_bin = label_binarize(self.y_test, classes=np.unique(self.y_test))
+        n_classes = y_bin.shape[1]
+
+        # Compute ROC curve and ROC area for each class
+        y_score = classifier.predict_proba(self.X_test)
+
+        # Compute ROC curve and ROC area for each class
+        fpr, tpr, roc_auc = dict(), dict(), dict()
+
+        for i in range(n_classes):
+            fpr[i], tpr[i], _ = roc_curve(y_bin[:, i], y_score[:, i])
+            roc_auc[i] = auc(fpr[i], tpr[i])
+
+        # Aggregate all false positive rates
+        all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+        # Interpolate all ROC curves at these points
+        mean_tpr = np.zeros_like(all_fpr)
+        for i in range(n_classes):
+            mean_tpr += np.interp(all_fpr, fpr[i], tpr[i])
+
+        # Average it and compute AUC
+        mean_tpr /= n_classes
+
+        # Plot all ROC curves
+        plt.figure(figsize=(8, 6))
+        colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'pink', 'lightgreen', 'lightblue',
+                  'gray']
+        for i, color in zip(range(n_classes), colors):
+            plt.plot(fpr[i], tpr[i], color=color, lw=2,
+                     label=f'ROC of {class_names[i]} (AUC = {roc_auc[i]:0.2f})')
+
+        plt.plot([0, 1], [0, 1], 'k--', lw=2)
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title(f'Multi-Class ROC for {name_model}')
+        plt.legend(loc="lower right")
+        plt.tight_layout()
+
+        # Save the ROC curve
+        if fine_turning is False:
+            roc_curve_filename = os.path.join(output_folder, f"{name_model}_multi_class_roc_curve.png")
+        else:
+            roc_curve_filename = os.path.join(output_folder, f"FT_{name_model}_multi_class_roc_curve.png")
+        plt.savefig(roc_curve_filename)
+        plt.close()
+        print(f'Done with {name_model}')
+
     def core_models(self, name_model, clf):
         start_time = time.time()
         text_clf = clf.fit(self.X_train, self.y_train)
         train_time = time.time() - start_time
-        print(f'Done training {name_model} in', train_time, 'seconds.')
         y_pred = text_clf.predict(self.X_test)
-        print(f'{name_model}, Accuracy =', metrics.accuracy_score(self.y_test, y_pred))
-        print(metrics.classification_report(self.y_test, y_pred, target_names=list(self.label_encoder.classes_)))
+        accuracy = metrics.accuracy_score(self.y_test, y_pred)
+        class_names = list(self.label_encoder.classes_)
+        report = metrics.classification_report(self.y_test, y_pred, target_names=class_names)
+        output_folder = '/home/sotatek/PycharmProjects/disertation_nlp/output'
+        report_filename = os.path.join(output_folder, f"{name_model}_classification_report.txt")
+        with open(report_filename, 'w') as file:
+            file.write(f"{name_model} Training Time: {train_time} seconds\n\n")
+            file.write(f'{name_model}, Accuracy ={accuracy}\n')
+            file.write("Classification Report:\n")
+            file.write(report)
 
-    def core_models_hyperparams(self, name_model, clf, param_grid):
+        # create a confusion matrix of model and save it
+        labels = list(self.label_encoder.classes_)
+        cm = metrics.confusion_matrix(self.y_test, y_pred)
+        fig_size = max(8, len(labels) / 3)
+        plt.figure(figsize=(fig_size, fig_size))
+        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=labels,
+                    yticklabels=labels)
+        plt.title(f'Confusion Matrix for {name_model}')
+        plt.ylabel('True Label')
+        plt.xlabel('Predicted Label')
+        # Save confusion matrix
+        cm_image_filename = os.path.join(output_folder, f"{name_model}_confusion_matrix.png")
+        plt.savefig(cm_image_filename, bbox_inches='tight')
+        plt.close()
+
+        """ Create ROC Curve"""
+        # Binarize the labels for multi-class ROC curve
+        y_bin = label_binarize(self.y_test, classes=np.unique(self.y_test))
+        n_classes = y_bin.shape[1]
+
+        # Compute ROC curve and ROC area for each class
+        y_score = clf.predict_proba(self.X_test)
+
+        # Compute ROC curve and ROC area for each class
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
+        for i in range(n_classes):
+            fpr[i], tpr[i], _ = roc_curve(y_bin[:, i], y_score[:, i])
+            roc_auc[i] = auc(fpr[i], tpr[i])
+
+        # Aggregate all false positive rates
+        all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+        # Interpolate all ROC curves at these points
+        mean_tpr = np.zeros_like(all_fpr)
+        for i in range(n_classes):
+            mean_tpr += np.interp(all_fpr, fpr[i], tpr[i])
+
+        # Average it and compute AUC
+        mean_tpr /= n_classes
+
+        # Plot all ROC curves
+        plt.figure(figsize=(8, 6))
+        colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'pink', 'lightgreen', 'lightblue',
+                  'gray']
+        for i, color in zip(range(n_classes), colors):
+            plt.plot(fpr[i], tpr[i], color=color, lw=2,
+                     label=f'ROC of {class_names[i]} (AUC = {roc_auc[i]:0.2f})')
+
+        plt.plot([0, 1], [0, 1], 'k--', lw=2)
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title(f'Multi-Class ROC for {name_model}')
+        plt.legend(loc="lower right")
+        plt.tight_layout()
+
+        # Save the ROC curve
+        roc_curve_filename = os.path.join(output_folder, f"{name_model}_multi_class_roc_curve.png")
+        plt.savefig(roc_curve_filename)
+        plt.close()
+        print(f'Done with {name_model}')
+
+    def core_models_hyperparams(self, name_model, clf, param_grid,
+                                output_folder='/home/sotatek/PycharmProjects/disertation_nlp/output'):
         start_time = time.time()
         grid_search = GridSearchCV(clf, param_grid, cv=5, scoring='accuracy')
         grid_search.fit(self.X_train, self.y_train)
@@ -158,6 +328,17 @@ class NewsClassification:
         print("Best cross-validation score: {:.2f}".format(grid_search.best_score_))
         print("Test set accuracy: {:.2f}".format(grid_search.score(self.X_test, self.y_test)))
 
+        # Save the best model
+        model_filename = os.path.join(output_folder, f"{name_model}_best_model.joblib")
+        dump(grid_search.best_estimator_, model_filename)
+        print(f"Best model saved as {model_filename}")
+
+        # Optionally, save the best parameters in a separate file
+        params_filename = f"{name_model}_best_params.txt"
+        with open(params_filename, 'w') as file:
+            file.write(str(grid_search.best_params_))
+        print(f"Best parameters saved as {params_filename}")
+
     def models_naive_bayes(self):
         text_clf = Pipeline([('vect', CountVectorizer(ngram_range=(1, 1),
                                                       max_df=0.8,
@@ -166,6 +347,21 @@ class NewsClassification:
                              ('clf', MultinomialNB())
                              ])
         self.core_models('Naive Bayes', text_clf)
+
+    def model_naive_bayes_hyperparameters(self):
+        text_clf = Pipeline([
+            ('vect', CountVectorizer()),
+            ('tfidf', TfidfTransformer()),
+            ('clf', MultinomialNB())
+        ])
+        param_grid = {
+            'vect__ngram_range': [(1, 1)],
+            'vect__max_df': [0.7, 0.8, 0.9],
+            'tfidf__use_idf': [True, False],
+            'clf__alpha': [1e-2, 1e-3, 1e-1, 1.0],
+            'clf__fit_prior': [True, False],
+        }
+        self.core_models_hyperparams('Naive Bayes', text_clf, param_grid)
 
     def models_naive_bayes_hyperparams(self):
 
@@ -198,7 +394,7 @@ class NewsClassification:
                                                       max_df=0.8,
                                                       max_features=None)),
                              ('tfidf', TfidfTransformer()),
-                             ('clf', SVC(gamma='scale'))
+                             ('clf', SVC(gamma='scale', probability=True))
                              ])
         self.core_models('SVM', text_clf)
 
@@ -216,7 +412,7 @@ class NewsClassification:
                                                       max_features=None)),
                              ('clf', RandomForestClassifier(max_depth=100, random_state=0))
                              ])
-        self.core_models('Decision Tree', text_clf)
+        self.core_models('Random Forest', text_clf)
 
     def model_xgboost(self):
         text_clf = Pipeline([
